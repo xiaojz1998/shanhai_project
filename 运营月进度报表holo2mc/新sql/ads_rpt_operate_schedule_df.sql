2a17e49ac19474aa9079211a9d37b271f9bda90f
xiaojz <xiaojz1998@outlook.com>
2025-05-24 18:27:49 +0800

    2025/5/24 做push报表修改任务

---
+++ 运营月进度报表holo2mc/新sql/ads_rpt_operate_schedule_df.sql
@@ -0,0 +1,17 @@
+---------------------------------------------
+-- File: ads_rpt_operate_schedule_df.sql
+-- Time: 2025/5/24 15:57
+-- User: xiaoj
+-- Description:  
+---------------------------------------------
+
+-------------------------------------------
+-- 建 mcstv.ads_rpt_operate_schedule 表
+-------------------------------------------
+-- drop table if exists mcstv.ads_rpt_operate_schedule;
+create table if not exists mcstv.ads_rpt_operate_schedule
+(
+
+);
+
+select now()
